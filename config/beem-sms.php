64b264a7f1f354387f7c3c810d531b2e80e9ca7b
James E. Msenga <hello@jamesemanuel.me>
2022-07-03 22:23:04 +0300

    BeemSms Class

--- config/beem-sms.php
+++ config/beem-sms.php
@@ -1,5 +1,11 @@
 <?php
-// config for Emanate/BeemSms
-return [
 
+return [
+    'api_key' => env('BEEM_SMS_API_KEY', ''),
+
+    'secret_key' => env('BEEM_SMS_SECRET_KEY', ''),
+
+    'sender_name' => env('BEEM_SMS_SENDER_NAME', 'INFO'),
+
+    'api_url' => env('BEEM_SMS_API_URL', 'https://apisms.beem.africa/v1/send'),
 ];
--- src/BeemSms.php
+++ src/BeemSms.php
@@ -4,4 +4,54 @@ namespace Emanate\BeemSms;
 
 class BeemSms
 {
+    public string $apiUrl;
+    public string $apiKey;
+    public string $secretKey;
+    public string $senderName;
+
+    public function __construct(array $config)
+    {
+        $this->apiUrl = $config['api_url'];
+        $this->apiKey = $config['api_key'];
+        $this->secretKey = $config['secret_key'];
+        $this->senderName = $config['sender_name'];
+    }
+
+    public function sendMessage()
+    {
+        $curl = curl_init();
+
+        $payload = [
+            'source_addr' => $this->senderName,
+            'encoding' => 0,
+            'schedule_time' => '',
+            'message' => 'Test SMS',
+            'recipients' => [
+                array('recipient_id' => '1', 'dest_addr' => '255656791558')
+            ]
+        ];
+
+        $ch = curl_init($this->apiUrl);
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt_array($ch, array(
+            CURLOPT_POST => TRUE,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_HTTPHEADER => array(
+                'Authorization:Basic ' . base64_encode("$this->apiKey:$this->secretKey"),
+                'Content-Type: application/json'
+            ),
+            CURLOPT_POSTFIELDS => json_encode($payload)
+        ));
+
+        $response = curl_exec($ch);
+
+        $results = json_decode($response, true, 512, 4194304);
+
+        curl_close($curl);
+
+        dd($results);
+    }
 }
