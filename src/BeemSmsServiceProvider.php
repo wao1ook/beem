4abb9b118c13148c9e6caf043dc78675bfe05bcd
James E. Msenga <hello@jamesemanuel.me>
2022-07-05 19:19:13 +0300

    Cleaning

--- src/BeemSmsServiceProvider.php
+++ src/BeemSmsServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Emanate\BeemSms;
 
-use Emanate\BeemSms\Commands\BeemSmsCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -10,16 +9,8 @@ class BeemSmsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('beem-sms')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_beem-sms_table')
-            ->hasCommand(BeemSmsCommand::class);
+            ->hasConfigFile();
     }
 }
--- tests/Pest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Emanate\BeemSms\Tests\TestCase;
-
-uses(TestCase::class)->in(__DIR__);
