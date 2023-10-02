6092734407aad58f9d4bd2ebc9efdd32f15c930c
Calvin Jackson <amigohmccclurkin98@gmail.com>
2023-10-02 21:29:20 +0300

    wip: added a new validator class

---
+++ src/Contracts/Validator.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Emanate\BeemSms\Contracts;
+
+interface Validator
+{
+    /**
+     * Create a new Validator instance.
+     *
+     * @param  array  $phoneAddresses
+     * @return Validator
+     */
+    public function make(array $phoneAddresses): Validator;
+
+    /**
+     * Run the validator's logic against the phone addresses.
+     */
+    public function validate(): array;
+}
