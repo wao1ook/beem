60812a13d349fc4e9164a98ce08334b68e2694f3
Calvin Jackson <amigohmccclurkin98@gmail.com>
2023-10-09 12:56:20 +0300

    chore: refactoring the config

--- config/beem.php
+++ config/beem.php
@@ -9,9 +9,15 @@ return [
 
     'sender_name' => env('BEEM_SMS_SENDER_NAME', 'INFO'),
 
-    'debug' => true,
-
+    /*
+     * If set to true, the phone addresses will be validated before sending the SMS.
+     * This will throw an exception if the phone number is invalid.
+     * Set it to false, if you don't want phone addresses validation.
+     */
     'validate_phone_addresses' => true,
 
+    /*
+     * Beem Sms Sending SMS URL. You can change this if you can use a different URL.
+     */
     'sending_sms_url' => 'https://apisms.beem.africa/v1/send',
 ];
